06c3cf25c57184a2c98cf33ac5fa41f8c6f46195
Matthew Miller <ginger424901@gmail.com>
2018-01-10 16:35:34 -0500

    gittest

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("github test");
+            string test = "test";
+            if(Console.ReadLine() == test)
+            {
+                Console.WriteLine("test was correct");
+                Console.ReadKey();
+            }
+        }
+    }
+}
